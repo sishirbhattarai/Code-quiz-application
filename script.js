5f674fa76b9f66f16210d19e614785c69fade57e
Sishir <sishir.bhattarai@outlook.com>
2020-10-10 22:39:41 -0600

    fixed the code and added html and js files

--- script.js
+++ script.js
@@ -1,87 +1,170 @@
-var questions = [{
-    question: "1. How do you write 'Hello World' in an alert box?",
-    choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
-    correctAnswer: 3
-}, {
-    question: "2. How to empty an array in JavaScript?",
-    choices: ["arrayList[]", "arrayList(0)", "arrayList.length=0", "arrayList.len(0)"],
-    correctAnswer: 2
-}, {
-    question: "3. What function to add an element at the begining of an array and one at the end?",
-    choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
-    correctAnswer: 1
-}, {
-    question: "4. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
-    choices: ["undefined", "0", "prints nothing", "Syntax error"],
-    correctAnswer: 0
-}, {
-    question: "5. What would following code return? console.log(typeof typeof 1);",
-    choices: ["string", "number", "Syntax error", "undefined"],
-    correctAnswer: 0
-},{
-	question: "6. Which software company developed JavaScript?",
-    choices: ["Mozilla", "Netscape", "Sun Microsystems", "Oracle"],
-    correctAnswer: 1
-},{
-	question: "7. What would be the result of 3+2+'7'?",
-    choices: ["327", "12", "14", "57"],
-    correctAnswer: 3
-},{
-	question: "8. Look at the following selector: $('div'). What does it select?",
-    choices: ["The first div element", "The last div element", "All div elements", "Current div element"],
-    correctAnswer: 2
-},{
-	question: "9. How can a value be appended to an array?",
-    choices: ["arr(length).value;", "arr[arr.length]=value;", "arr[]=add(value);", "None of these"],
-    correctAnswer: 1
-},{
-	question: "10. What will the code below output to the console? console.log(1 +  +'2' + '2');",
-    choices: ["'32'", "'122'", "'13'", "'14'"],
-    correctAnswer: 0
-}];
+var myScore = document.getElementById("score");
+var btnScore = document.getElementById("btnScore");
+var currentindex = 0;
+var score = 0;
+var count = 120;
+var alert =document.getElementById("alert");
+var info = document.getElementById("info");
+var quizQuestions = document.getElementById("quiz-questions");
+var timer = document.getElementById("timer");
+var btnStart = document.getElementById("btn-start");
+var timecounter = document.getElementById("timecounter");
+var titleitem = document.getElementById("title-item");
+var nextQuestions
+var questionanswers = document.getElementById("question-answers");
+const MAX_QUESTIONS = 7
 
-var questionAnswers = $("#question-answers")
-questions[0].choices.forEach(function(choice){
-    var buttonEl = $("<button>"+choice+"</button>")
-    buttonEl.on("click",function(){
-        console.log($(this).text())
-        var correctAnswer = questions[0].correctAnswer
-        if(questions[0].choices[correctAnswer]=== $(this).text()){
-        console.log("correct")
-        }
+// var addscore = document.getElementById("addscore");
+// var submitresult = document.getElementById("submitresult");
+var allScores = [];
+var storedScores = JSON.parse(localStorage.getItem("userData"));
+var questions = [
+    {
+        title: '1. How to empty an array in JavaScript?',
+        choices: ["arrayList[]","arrayList(0)","arrayList.length=0", "arrayList.len(0)"],
+        answer : "arrayList.length=0"    
+    },
+    {
+        title: '2. The condition in an if/else statement is enclosed within:---',
+        choices: ["quotes","Curly brackets","parentheses", "square brackets"],
+        answer : "parentheses"    
+    },
+    {
+        title: '3. A very useful tool used during development and debugging for printing content to the debugger is:---',
+        choices: ["JavaScript","terminal/bash","alerts", "console.log"],
+        answer : "console.log"    
+    },
+    {
+        title: '4. What will this output? var a = [1, 2, 3]; console.log(a[6]);',
+        choices: ["undefined","0","Syntaxerror","prints Nothing"],
+        answer : "undefined"    
+    },
+    {
+        title: "5. Look at the following selector: $('div'). What does it select?",
+        choices: ["The first div element","The last div element","All div elements", "Current div element"],
+        answer : "All div elements"    
+    },
+    {
+        title: "6. What would be the result of 3+2+'7'?",
+        choices: ["327","12","15", "57"],
+         answer : "57" 
+     },
+     {
+        title: "7. How do you write 'Hello World' in an alert box?",
+        choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
+         answer : "alert('Hello World');"
+     }
 
-        else{
-            console.log("Incorrect")
-        }
-    })
-    questionAnswers.append(buttonEl)
-})
+]
+btnStart.addEventListener("click", starQuiz);
+function starQuiz(){
+    
+    document.getElementsByClassName("code-title")[0].style.visibility = "hidden";
+    document.getElementsByClassName("col-lg-11")[0].style.visibility = "hidden";
+    document.getElementsByClassName("btnstart")[0].style.visibility = "hidden";
+    quizQuestions.classList.remove("d-none")
+    document.getElementById("title-item").textContent = questions[0].question
 
-/*
-1. start
-- make sure end screen and game screen is hidden
-- add event listner to start button
-  -Needs to hide start screen and show the gamescreen.
-  - Start timer and show current question
-  - If there is no time left, the timer should stop and you should end the game.
-2. game screen
-- Show current question and answer
-  - Each answer when clicked should check if correct answer was selected.
-  - If incorrect decrease the time remaining.
-  - If correct increase score. 
-  - Show next question.
-    - If there are no more questions, end the game.
+    console.log("I am cool")
+    nextQuestions= questions[currentindex]
+    
+    
+        displayQuestion(nextQuestions)
+
+    gametime()
+
+    if(storedScores !==null) {
+        allScores = storedScores;
+    }
   
-3. end screen 
- - Hide all other screen and show end screen 
- - Add event listner to initial submit button. SO, when clicked saves the user initials and scores to local storage
- - redirect users to high score page.
- 
+}
+btnScore.addEventListener("click" , function(){
+    let name = document.getElementById("inputScore").value
+    scorePage(name, count)
+});
+// Time set
 
-4. High Scores
-- retrive the list of high scores from local storage and show them on screen.
-- have a link to go back to main page
+function gametime(){
+
+    var timeinterval = setInterval(function(){
+        timer.innerText = count
+         count--;
+        }, 1000);
+
+}
+
+function scorePage(a, b) {
+
+    var userData = {
+        inits: a,
+        userScore: b
+    };
+    allScores.push(userData);
+
+    localStorage.setItem("userData", JSON.stringify(allScores));
+    location.href = "score.html";
+}
+
+function displayQuestion(question){
+    titleitem.innerText=question.title
+    question.choices.forEach(element => {
+     var button =document.createElement("button")
+    button.className="btn-primary btn-block text-left"
+    button.innerText=element
+    // questionanswers.innerHTML=""
+    questionanswers.appendChild(button)
+    button.addEventListener("click", displaynextQuestion)
+    });
+}
 
 
+function displaynextQuestion(e){
+    currentindex++
+    if(currentindex < questions.length){
+        correction(e.target.innerText == nextQuestions.answer)
+        questionanswers.innerHTML=""
+        if(currentindex < questions.length){    
+            nextQuestions= questions[currentindex]
+            displayQuestion(nextQuestions)  
+        }else {
+            currentindex = 0
+            displayQuestion(nextQuestions)  
+        }
 
-*/
+    }else{
+        console.log("endgame")
+        endgame()
+        
+
+    }
+    
+     
+}
+function correction(response){
+    
+    if(response){
+        alert.innerText= "Yay!! You are right"
+        
+    }else {
+        alert.innerText="Sorry, You are Wrong"
+        count = count -20
+        timer.innerHTML = count
+        
+
+    }
+    setTimeout(function(){
+        alert.innerText=""
+    
+        }, 1000);
+
+}
+ function endgame (){
+    // btnStart.classList.add("d-none")
+    myScore.innaText = count
+    scoreadd.classList.remove("d-none")
+    timecounter.classList.add("d-none")
+    quizQuestions.classList.add("d-none")
+    
+
+
+ }
