1db973940e08feb9f81d7fff22966b11eeef10b6
Sishir <sishir.bhattarai@outlook.com>
2020-10-12 23:51:46 -0600

    Modified some codes

--- script.js
+++ script.js
@@ -3,171 +3,186 @@ var btnScore = document.getElementById("btnScore");
 var currentQuestionIndex = 0;
 var score = 0;
 var count = 120;
-var alert =document.getElementById("alert");
+var alert = document.getElementById("alert");
 var info = document.getElementById("info");
 var quizQuestions = document.getElementById("quiz-questions");
 var timer = document.getElementById("timer");
 var btnStart = document.getElementById("btn-start");
 var timeCounter = document.getElementById("timecounter");
 var titleitem = document.getElementById("title-item");
-var nextQuestions
+var nextQuestions;
 var questionanswers = document.getElementById("question-answers");
-const MAX_QUESTIONS = 7
-
+const MAX_QUESTIONS = 7;
 
 var allScores = [];
 var storedScores = JSON.parse(localStorage.getItem("Data"));
 var questions = [
-    {
-        title: '1. How to empty an array in JavaScript?',
-        choices: ["arrayList[]","arrayList(0)","arrayList.length=0", "arrayList.len(0)"],
-        answer : "arrayList.length=0"    
-    },
-    {
-        title: '2. The condition in an if/else statement is enclosed within:---',
-        choices: ["quotes","Curly brackets","parentheses", "square brackets"],
-        answer : "parentheses"    
-    },
-    {
-        title: '3. A very useful tool used during development and debugging for printing content to the debugger is:---',
-        choices: ["JavaScript","terminal/bash","alerts", "console.log"],
-        answer : "console.log"    
-    },
-    {
-        title: '4. What will this output? var a = [1, 2, 3]; console.log(a[6]);',
-        choices: ["undefined","0","Syntaxerror","prints Nothing"],
-        answer : "undefined"    
-    },
-    {
-        title: "5. Look at the following selector: $('div'). What does it select?",
-        choices: ["The first div element","The last div element","All div elements", "Current div element"],
-        answer : "All div elements"    
-    },
-    {
-        title: "6. What would be the result of 3+2+'7'?",
-        choices: ["327","12","15", "57"],
-         answer : "57" 
-     },
-     {
-        title: "7. How do you write 'Hello World' in an alert box?",
-        choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
-         answer : "alert('Hello World');"
-     }
-
-]
-btnStart.addEventListener("click", quizStart);
+  {
+    title: "1. How to empty an array in JavaScript?",
+    choices: [
+      "arrayList[]",
+      "arrayList(0)",
+      "arrayList.length=0",
+      "arrayList.len(0)",
+    ],
+    answer: "arrayList.length=0",
+  },
+  {
+    title: "2. The condition in an if/else statement is enclosed within:---",
+    choices: ["quotes", "Curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title:
+      "3. A very useful tool used during development and debugging for printing content to the debugger is:---",
+    choices: ["JavaScript", "terminal/bash", "alerts", "console.log"],
+    answer: "console.log",
+  },
+  {
+    title: "4. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
+    choices: ["undefined", "0", "Syntaxerror", "prints Nothing"],
+    answer: "undefined",
+  },
+  {
+    title: "5. Look at the following selector: $('div'). What does it select?",
+    choices: [
+      "The first div element",
+      "The last div element",
+      "All div elements",
+      "Current div element",
+    ],
+    answer: "All div elements",
+  },
+  {
+    title: "6. What would be the result of 3+2+'7'?",
+    choices: ["327", "12", "15", "57"],
+    answer: "57",
+  },
+  {
+    title: "7. How do you write 'Hello World' in an alert box?",
+    choices: [
+      "msg('Hello World')",
+      "msgBox('Hello World');",
+      "alertBox('Hello World');",
+      "alert('Hello World');",
+    ],
+    answer: "alert('Hello World');",
+  },
+];
+if (btnStart) {
+  btnStart.addEventListener("click", quizStart);
+}
 /*Below are the things I need to execute once button is clicked*/
 
-function quizStart(){
-    
-    document.getElementsByClassName("code-title")[0].style.visibility = "hidden";
-    document.getElementsByClassName("col-lg-11")[0].style.visibility = "hidden";
-    document.getElementsByClassName("btnstart")[0].style.visibility = "hidden";
-    quizQuestions.classList.remove("d-none")
-    
+function quizStart() {
+  document.getElementsByClassName("code-title")[0].style.visibility = "hidden";
+  document.getElementsByClassName("col-lg-11")[0].style.visibility = "hidden";
+  document.getElementsByClassName("btnstart")[0].style.visibility = "hidden";
+  quizQuestions.classList.remove("d-none");
 
-    /*Console log for debuging*/
-    console.log("I am cool")
+  /*Console log for debuging*/
+  console.log("I am cool");
 
-    nextQuestions= questions[currentQuestionIndex]
-            showQuestion(nextQuestions)
+  nextQuestions = questions[currentQuestionIndex];
+  showQuestion(nextQuestions);
 
-    timertracker()
+  timertracker();
 
-    if(storedScores !==null) {
-        allScores = storedScores;
-    }
+  if (storedScores !== null) {
+    allScores = storedScores;
   }
+}
 
 // Time set
 
-function timertracker(){
-
-    var timeinterval = setInterval(function(){
-        timer.innerText = count
-         count--;
-        }, 1000);
-
+function timertracker() {
+  var timeinterval = setInterval(function () {
+    timer.innerText = count;
+    count--;
+  }, 1000);
 }
 
 function scorePage(a, b) {
+  var Data = {
+    inits: a,
+    userScore: b,
+  };
+  allScores.push(Data);
 
-    var Data = {
-        inits: a,
-        userScore: b
-    };
-    allScores.push(Data);
-
-    localStorage.setItem("Data", JSON.stringify(allScores));
-    location.href = "score.html";
+  localStorage.setItem("Data", JSON.stringify(allScores));
+  location.href = "score.html";
 }
 
+function showQuestion(question) {
+  titleitem.innerText = question.title;
 
-function showQuestion(question){
-    titleitem.innerText=question.title
-   
-    question.choices.forEach(element => {
-     var button =document.createElement("button")
-    button.className="btn-primary btn-block text-left"
-    button.innerText=element
+  question.choices.forEach((element) => {
+    var button = document.createElement("button");
+    button.className = "btn-primary btn-block text-left";
+    button.innerText = element;
     // questionanswers.innerHTML=""
-    questionanswers.appendChild(button)
-    button.addEventListener("click", showNextQuestion)
-    });
+    questionanswers.appendChild(button);
+    button.addEventListener("click", showNextQuestion);
+  });
 }
 
-
-function showNextQuestion(e){
-    currentQuestionIndex++
-    if(currentQuestionIndex < questions.length){
-        correct(e.target.innerText == nextQuestions.answer)
-        questionanswers.innerHTML=""
-        if(currentQuestionIndex < questions.length){    
-            nextQuestions= questions[currentQuestionIndex]
-            showQuestion(nextQuestions)  
-        }else {
-            currentQuestionIndex = 0
-            showQuestion(nextQuestions)  
-        }
-
-    }else{
-        console.log("you are done")
-        endgame()
-        
-
-    }    
+function showNextQuestion(e) {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    correct(e.target.innerText == nextQuestions.answer);
+    questionanswers.innerHTML = "";
+    if (currentQuestionIndex < questions.length) {
+      nextQuestions = questions[currentQuestionIndex];
+      showQuestion(nextQuestions);
+    } else {
+      currentQuestionIndex = 0;
+      showQuestion(nextQuestions);
+    }
+  } else {
+    done();
+  }
 }
 
+function done() {
+  console.log("you are done");
+
+  location.href = "submitbutton.html";
+
+  scorePage(a, b);
+
+  endgame();
+}
+if (btnScore) {
+  btnScore.addEventListener("click", submitQuiz);
+}
+function submitQuiz() {
+  let name = document.getElementById("inputScore").value;
+  scorePage(name, count);
+}
+
+/*
 btnScore.addEventListener("click" , function(){
     let name = document.getElementById("inputScore").value
     scorePage(name, count)
+    
 });
-
-function correct(response){
-    
-    if(response){
-        alert.innerText= "Yay!! You are right"
-        
-    }else {
-        alert.innerText="Sorry, You are Wrong"
-        count = count -20
-        timer.innerHTML = count
-        
-
-    }
-    setTimeout(function(){
-        alert.innerText=""
-    
-        }, 1000);
-
+*/
+function correct(response) {
+  if (response) {
+    alert.innerText = "Yay!! You are right";
+  } else {
+    alert.innerText = "Sorry, You are Wrong";
+    count = count - 20;
+    timer.innerHTML = count;
+  }
+  setTimeout(function () {
+    alert.innerText = "";
+  }, 1000);
 }
- function endgame (){
-    // btnStart.classList.add("d-none")
-    myScore.innaText = count
-    scoreadd.classList.remove("d-none")
-    timeCounter.classList.add("d-none")
-    quizQuestions.classList.add("d-none")
-    
-
-
- }
+function endgame() {
+  // btnStart.classList.add("d-none")
+  myScore.innaText = count;
+  scoreadd.classList.remove("d-none");
+  timeCounter.classList.add("d-none");
+  quizQuestions.classList.add("d-none");
+}
